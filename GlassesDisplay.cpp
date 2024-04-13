e37e128cbfaede61441084612dbe2d549b8f5776
EPW80 <erikpw009@gmail.com>
2024-04-12 19:08:27 -0700

    functions need redo/catching seg fault

--- GlassesDisplay.cpp
+++ GlassesDisplay.cpp
@@ -4,41 +4,49 @@
 #include <stdexcept>
 #include "GlassesDisplay.hpp"
 
+using std::string;
+using std::ifstream;
 using std::cout;
 using std::endl;
-using std::ifstream;
-using std::string;
 
+// function to return the hash value based on the first digit
 unsigned int hashfct1(unsigned int barcode) {
-    return barcode % 10;
+  return (barcode / 1000000) % 10;
 }
 
+// function to return the hash value based on the second digit
 unsigned int hashfct2(unsigned int barcode) {
-    return (barcode / 10) % 10;
+  return (barcode / 100000) % 10;
 }
 
+// function to return the hash value based on the third digit
 unsigned int hashfct3(unsigned int barcode) {
-    return (barcode / 100) % 10;
+  return (barcode / 10000) % 10;
 }
 
+// function to return the hash value based on the fourth digit
 unsigned int hashfct4(unsigned int barcode) {
-    return (barcode / 1000) % 10;
+  return (barcode / 1000) % 10;
 }
 
+// function to return the hash value based on the fifth digit
 unsigned int hashfct5(unsigned int barcode) {
-    return (barcode / 10000) % 10;
+  return (barcode / 100) % 10;
 }
 
+// function to return the hash value based on the sixth digit
 unsigned int hashfct6(unsigned int barcode) {
-    return (barcode / 100000) % 10;
+  return (barcode / 10) % 10;
 }
 
+// function to return the hash value based on the seventh digit
 unsigned int hashfct7(unsigned int barcode) {
-    return (barcode / 1000000) % 10;
+  return barcode % 10;
 }
 
 // Constructor for struct Item
-Glasses::Glasses(string glassesColor, string glassesShape, string glassesBrand, unsigned int barcode) : glassesColor_(glassesColor), glassesShape_(glassesShape), glassesBrand_(glassesBrand), barcode_(barcode){};
+Glasses::Glasses(string glassesColor, string glassesShape, string glassesBrand, unsigned int barcode): glassesColor_(glassesColor), glassesShape_(glassesShape), glassesBrand_(glassesBrand), barcode_(barcode)
+{};
 
 // Load information from a text file with the given filename
 // THIS FUNCTION IS COMPLETE
@@ -52,28 +60,19 @@ void GlassesDisplay::readTextfile(string filename) {
     string glassesBrand;
     unsigned int barcode;
     while (myfile >> glassesColor >> glassesShape >> glassesBrand >> barcode) {
-      if (glassesColor.size() > 0)
-        addGlasses(glassesColor, glassesShape, glassesBrand, barcode);
+			if (glassesColor.size() > 0)
+      	addGlasses(glassesColor, glassesShape, glassesBrand, barcode);
     }
     myfile.close();
-  } else
+  }
+  else
     throw std::invalid_argument("Could not open file " + filename);
 }
 
 void GlassesDisplay::addGlasses(string glassesColor, string glassesShape, string glassesBrand, unsigned int barcode) {
-  if (glassesColor.empty() || glassesShape.empty() || glassesBrand.empty() || barcode == 0) {
-        throw std::invalid_argument("Invalid glasses information");
-    }
-
     Glasses new_glasses(glassesColor, glassesShape, glassesBrand, barcode);
-    hT1[barcode] = new_glasses;
-    hT2[barcode] = new_glasses;
-    hT3[barcode] = new_glasses;
-    hT4[barcode] = new_glasses;
-    hT5[barcode] = new_glasses;
-    hT6[barcode] = new_glasses;
-    hT7[barcode] = new_glasses;
 }
+
 
 bool GlassesDisplay::removeGlasses(unsigned int barcode) {
 
@@ -83,9 +82,10 @@ unsigned int GlassesDisplay::bestHashing() {
 
 }
 
+
 // ALREADY COMPLETED
 size_t GlassesDisplay::size() {
-  if ((hT1.size() != hT2.size()) || (hT1.size() != hT3.size()) || (hT1.size() != hT4.size()) || (hT1.size() != hT5.size()) || (hT1.size() != hT6.size()) || (hT1.size() != hT7.size()))
+  if ((hT1.size() != hT2.size()) || (hT1.size() != hT3.size()) || (hT1.size() != hT4.size()) || (hT1.size() != hT5.size())|| (hT1.size() != hT6.size()) || (hT1.size() != hT7.size()))
     throw std::length_error("Hash table sizes are not the same");
-  return hT1.size();
+	return hT1.size();
 }
--- GlassesDisplay.hpp
+++ GlassesDisplay.hpp
@@ -1,5 +1,4 @@
 #pragma once
-
 #include <string>
 #include <unordered_map>
 using std::string;
@@ -9,12 +8,12 @@ using std::string;
 //******************************
 
 // struct to store all the info about a particular pair of glasses
-struct Glasses {
+struct Glasses{
   string glassesColor_;
   string glassesShape_;
   string glassesBrand_;
   unsigned int barcode_;
-  Glasses(string glassesColor = "", string glassesShape = "", string glassesBrand = "", unsigned int barcode = 1000000);
+  Glasses(string glassesColor="", string glassesShape="", string glassesBrand="", unsigned int barcode=1000000);
 };
 
 // TO BE COMPLETED: unary function to return the hash value based on
@@ -49,6 +48,7 @@ unsigned int hashfct7(unsigned int);
 // Typedef for custom hash table
 //******************************
 typedef std::unordered_map<unsigned int, Glasses, decltype(&hashfct1)> CustomHashTable;
+
 
 // class to store the bow collection
 class GlassesDisplay {

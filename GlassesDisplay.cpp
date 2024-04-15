7b1dd6daf754ba0c1324691cc7c4679e005a66ce
EPW80 <erikpw009@gmail.com>
2024-04-14 20:39:29 -0700

    function refactor

--- GlassesDisplay.cpp
+++ GlassesDisplay.cpp
@@ -1,15 +1,7 @@
-#include <algorithm>
-
-#include <limits>
-
 #include <string>
-
 #include <iostream>
-
 #include <fstream>
-
 #include <stdexcept>
-
 #include "GlassesDisplay.hpp"
 
 using std::cout;
@@ -17,167 +9,143 @@ using std::endl;
 using std::ifstream;
 using std::string;
 
-unsigned int hashfct1(unsigned int barcode)
-{
+unsigned int hashfct1(unsigned int barcode) {
   return (barcode / 1000000) % 10;
 }
 
-unsigned int hashfct2(unsigned int barcode)
-{
+unsigned int hashfct2(unsigned int barcode) {
   return (barcode / 100000) % 10;
 }
 
-unsigned int hashfct3(unsigned int barcode)
-{
+unsigned int hashfct3(unsigned int barcode) {
   return (barcode / 10000) % 10;
 }
 
-unsigned int hashfct4(unsigned int barcode)
-{
+unsigned int hashfct4(unsigned int barcode) {
   return (barcode / 1000) % 10;
 }
 
-unsigned int hashfct5(unsigned int barcode)
-{
+unsigned int hashfct5(unsigned int barcode) {
   return (barcode / 100) % 10;
 }
 
-unsigned int hashfct6(unsigned int barcode)
-{
+unsigned int hashfct6(unsigned int barcode) {
   return (barcode / 10) % 10;
 }
 
-unsigned int hashfct7(unsigned int barcode)
-{
+unsigned int hashfct7(unsigned int barcode) {
   return barcode % 10;
 }
-Glasses::Glasses(string glassesColor, string glassesShape, string glassesBrand, unsigned int barcode) : glassesColor_(glassesColor), glassesShape_(glassesShape), glassesBrand_(glassesBrand), barcode_(barcode){};
-void GlassesDisplay::readTextfile(string filename)
-{
+Glasses::Glasses(string glassesColor, string glassesShape, string glassesBrand, unsigned int barcode): glassesColor_(glassesColor), glassesShape_(glassesShape), glassesBrand_(glassesBrand), barcode_(barcode) {};
+void GlassesDisplay::readTextfile(string filename) {
   ifstream myfile(filename);
 
-  if (myfile.is_open())
-  {
+  if (myfile.is_open()) {
     cout << "Successfully opened file " << filename << endl;
     string glassesColor;
     string glassesShape;
     string glassesBrand;
     unsigned int barcode;
-    while (myfile >> glassesColor >> glassesShape >> glassesBrand >> barcode)
-    {
+    while (myfile >> glassesColor >> glassesShape >> glassesBrand >> barcode) {
       if (glassesColor.size() > 0)
         addGlasses(glassesColor, glassesShape, glassesBrand, barcode);
     }
     myfile.close();
-  }
-  else
+  } else
     throw std::invalid_argument("Could not open file " + filename);
 }
 
-void GlassesDisplay::addGlasses(string glassesColor, string glassesShape, string glassesBrand, unsigned int barcode)
-{
+void GlassesDisplay::addGlasses(string glassesColor, string glassesShape, string glassesBrand, unsigned int barcode) {
   Glasses new_glasses(glassesColor, glassesShape, glassesBrand, barcode);
 
-  if (hashfct1(barcode) < hT1.size())
-  {
+  if (hashfct1(barcode) < hT1.size()) {
     hT1[hashfct1(barcode)] = new_glasses;
   }
-  if (hashfct2(barcode) < hT2.size())
-  {
+  if (hashfct2(barcode) < hT2.size()) {
     hT2[hashfct2(barcode)] = new_glasses;
   }
-  if (hashfct3(barcode) < hT3.size())
-  {
+  if (hashfct3(barcode) < hT3.size()) {
     hT3[hashfct3(barcode)] = new_glasses;
   }
-  if (hashfct4(barcode) < hT4.size())
-  {
+  if (hashfct4(barcode) < hT4.size()) {
     hT4[hashfct4(barcode)] = new_glasses;
   }
-  if (hashfct5(barcode) < hT5.size())
-  {
+  if (hashfct5(barcode) < hT5.size()) {
     hT5[hashfct5(barcode)] = new_glasses;
   }
-  if (hashfct6(barcode) < hT6.size())
-  {
+  if (hashfct6(barcode) < hT6.size()) {
     hT6[hashfct6(barcode)] = new_glasses;
   }
-  if (hashfct7(barcode) < hT7.size())
-  {
+  if (hashfct7(barcode) < hT7.size()) {
     hT7[hashfct7(barcode)] = new_glasses;
   }
 }
 
-bool GlassesDisplay::removeGlasses(unsigned int barcode)
-{
-  if (hashfct1(barcode) < hT1.size())
-  {
+bool GlassesDisplay::removeGlasses(unsigned int barcode) {
+  if (hashfct1(barcode) < hT1.size()) {
     hT1.erase(hashfct1(barcode));
   }
-  if (hashfct2(barcode) < hT2.size())
-  {
+  if (hashfct2(barcode) < hT2.size()) {
     hT2.erase(hashfct2(barcode));
   }
-  if (hashfct3(barcode) < hT3.size())
-  {
+  if (hashfct3(barcode) < hT3.size()) {
     hT3.erase(hashfct3(barcode));
   }
-  if (hashfct4(barcode) < hT4.size())
-  {
+  if (hashfct4(barcode) < hT4.size()) {
     hT4.erase(hashfct4(barcode));
   }
-  if (hashfct5(barcode) < hT5.size())
-  {
+  if (hashfct5(barcode) < hT5.size()) {
     hT5.erase(hashfct5(barcode));
   }
-  if (hashfct6(barcode) < hT6.size())
-  {
+  if (hashfct6(barcode) < hT6.size()) {
     hT6.erase(hashfct6(barcode));
   }
-  if (hashfct7(barcode) < hT7.size())
-  {
+  if (hashfct7(barcode) < hT7.size()) {
     hT7.erase(hashfct7(barcode));
   }
   return true;
 }
 
-unsigned int GlassesDisplay::bestHashing()
-{
-    CustomHashTable* hashTables[7] = {&hT1, &hT2, &hT3, &hT4, &hT5, &hT6, &hT7};
-    unsigned int bestHashTableIndex = 0;
-    size_t minBalance = std::numeric_limits<size_t>::max();
+unsigned int GlassesDisplay::bestHashing() {
+  CustomHashTable * hash_tables[7] = {
+    & hT1,
+    & hT2,
+    & hT3,
+    & hT4,
+    & hT5,
+    & hT6,
+    & hT7
+  };
 
-    for (unsigned int i = 0; i < 7; ++i) {
-        size_t maxBucketSize = 0;
-        size_t minBucketSize = std::numeric_limits<size_t>::max();
-        unsigned int numBucketsToCheck = std::min(static_cast<size_t>(10), hashTables[i]->bucket_count());
+  unsigned int best_hash_table_index = 0;
+  size_t min_balance = -1; 
 
-        for (unsigned int j = 0; j < numBucketsToCheck; ++j) {
-            size_t bucketSize = hashTables[i]->bucket_size(j);
-            maxBucketSize = std::max(maxBucketSize, bucketSize);
-            minBucketSize = std::min(minBucketSize, bucketSize);
-        }
+  for (unsigned int i = 0; i < 7; ++i) {
+    size_t max_bucket_size = 0;
+    size_t min_bucket_size = -1; 
+    unsigned int bucket_check = (hash_tables[i] -> bucket_count() < 10) ? hash_tables[i] -> bucket_count() : 10;
 
-        size_t balance = maxBucketSize - minBucketSize;
-        if (balance < minBalance) {
-            minBalance = balance;
-            bestHashTableIndex = i; 
-        }
+    for (unsigned int j = 0; j < bucket_check; ++j) {
+      size_t bucket_size = hash_tables[i] -> bucket_size(j);
+      max_bucket_size = (bucket_size > max_bucket_size) ? bucket_size : max_bucket_size;
+      min_bucket_size = (bucket_size < min_bucket_size) ? bucket_size : min_bucket_size;
     }
 
-    return bestHashTableIndex + 1; 
+    size_t balance = max_bucket_size - min_bucket_size;
+    if (balance < min_balance) {
+      min_balance = balance;
+      best_hash_table_index = i;
+    }
+  }
+
+  return best_hash_table_index + 1;
 }
 
-
-size_t GlassesDisplay::size()
-{
+size_t GlassesDisplay::size() {
   // Check if the sizes of the hash tables are equal
-  if (hT1.size() == hT2.size() && hT1.size() == hT3.size() && hT1.size() == hT4.size() && hT1.size() == hT5.size() && hT1.size() == hT6.size() && hT1.size() == hT7.size())
-  {
+  if (hT1.size() == hT2.size() && hT1.size() == hT3.size() && hT1.size() == hT4.size() && hT1.size() == hT5.size() && hT1.size() == hT6.size() && hT1.size() == hT7.size()) {
     return hT1.size();
-  }
-  else
-  {
+  } else {
     throw std::length_error("Hash table sizes are not the same");
   }
 }
